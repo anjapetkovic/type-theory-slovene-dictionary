5f5602bd9c139bf4aedaa101adc9ae6d4f9d4679
Anja Petković <anja.petkovic@gmail.com>
2020-11-17 10:02:27 +0100

    basic dictionary

---
+++ sort.py
@@ -0,0 +1,16 @@
+dictionary_file = "dictionary.md"
+dictionary = {}
+
+with open(dictionary_file, "r") as f:
+    for line in f:
+        if "=" in line:
+            p = line.split("=")
+            dictionary[p[0]] = p[1]
+        else:
+            continue
+
+sorted_keys = sorted(list(dictionary.keys()))
+
+with open(dictionary_file, "w") as f:
+    for k in sorted_keys:
+        print(k + "=" + dictionary[k], file=f, end="")
